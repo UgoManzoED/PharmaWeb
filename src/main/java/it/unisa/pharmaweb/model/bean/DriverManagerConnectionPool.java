fad36e69c52ac1385fdb2e38dc602c89ea796a8b
UgoManzoED <u.manzo2@studenti.unisa.it>
2025-10-18 07:38:30 +0200

    conf: Creazione classe per gestione con il DB

---
+++ src/main/java/it/unisa/pharmaweb/model/bean/DriverManagerConnectionPool.java
@@ -0,0 +1,29 @@
+package it.unisa.pharmaweb.model.bean;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DriverManagerConnectionPool {
+	
+	// --- Dati di configurazione del Database ---
+    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/pharmaweb_db?serverTimezone=UTC";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "TSWunisa123";
+
+    // --- Caricamento driver JDBC di MySQL. ---
+    static {
+        try {
+            Class.forName(JDBC_DRIVER);
+        } catch (ClassNotFoundException e) {
+            System.err.println("Errore nel caricamento del driver JDBC: " + e.getMessage());
+            throw new RuntimeException("Failed to load MySQL JDBC driver", e);
+        }
+    }
+
+    // --- Connessione al Database
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASSWORD); // Ogni volta che viene chiamato, crea una nuova connessione
+    }
+}
