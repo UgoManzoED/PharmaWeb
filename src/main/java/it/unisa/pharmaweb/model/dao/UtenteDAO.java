e8d10ab6642576be3fc2324aa43ee594f274ba60
UgoManzoED <u.manzo2@studenti.unisa.it>
2025-10-20 20:08:08 +0200

    fix: aggiunta librerie mancanti

--- src/main/java/it/unisa/pharmaweb/model/dao/UtenteDAO.java
+++ src/main/java/it/unisa/pharmaweb/model/dao/UtenteDAO.java
@@ -5,6 +5,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.mindrot.jbcrypt.BCrypt;
 
 public class UtenteDAO {
