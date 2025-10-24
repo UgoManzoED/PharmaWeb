ad55eecb9024510290fa8b4563a5f21f2e0fd347
UgoManzoED <u.manzo2@studenti.unisa.it>
2025-10-24 08:41:39 +0200

    funz: Logica Dashboard migliorata

--- src/main/java/it/unisa/pharmaweb/control/DashboardServlet.java
+++ src/main/java/it/unisa/pharmaweb/control/DashboardServlet.java
@@ -3,6 +3,7 @@ package it.unisa.pharmaweb.control;
 import it.unisa.pharmaweb.model.bean.OrdineBean;
 import it.unisa.pharmaweb.model.bean.UtenteBean;
 import it.unisa.pharmaweb.model.dao.IndirizzoDAO;
+import it.unisa.pharmaweb.model.dao.MetodoPagamentoDAO;
 import it.unisa.pharmaweb.model.dao.OrdineDAO;
 import java.io.IOException;
 import java.util.List;
@@ -22,15 +23,20 @@ public class DashboardServlet extends HttpServlet {
         HttpSession session = request.getSession(false);
         UtenteBean utente = (UtenteBean) session.getAttribute("utente");
 
-        // Recupera lo storico degli ordini dell'utente
+        // Prepara i dati per le varie sezioni della dashboard
         OrdineDAO ordineDAO = new OrdineDAO();
-        List<OrdineBean> storicoOrdini = ordineDAO.getOrdiniByUtente(utente.getIdUtente());
-
-        // Aggiungi lo storico alla richiesta per passarlo alla JSP
-        request.setAttribute("storicoOrdini", storicoOrdini);
-        
         IndirizzoDAO indirizzoDAO = new IndirizzoDAO();
+        MetodoPagamentoDAO pagamentoDAO = new MetodoPagamentoDAO();
+
+        // Recupera lo storico degli ordini
+        List<OrdineBean> storicoOrdini = ordineDAO.getOrdiniByUtente(utente.getIdUtente());
+        request.setAttribute("storicoOrdini", storicoOrdini);
+
+        // Recupera gli indirizzi salvati
         request.setAttribute("indirizzi", indirizzoDAO.getAllByUserId(utente.getIdUtente()));
+        
+        // Recupera i metodi di pagamento salvati
+        request.setAttribute("pagamenti", pagamentoDAO.getAllByUserId(utente.getIdUtente()));
 
         // Inoltra alla pagina JSP della dashboard
         RequestDispatcher dispatcher = request.getRequestDispatcher("/dashboard.jsp");
