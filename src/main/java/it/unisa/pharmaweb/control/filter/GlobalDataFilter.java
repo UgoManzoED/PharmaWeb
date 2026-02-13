9174e0523ee92007da902dfc54bf7699fb034fe5
UgoManzoED <u.manzo2@studenti.unisa.it>
2026-02-13 19:21:43 +0100

    funz: implementa GlobalDataFilter per rendere le recensioni del footer globali

---
+++ src/main/java/it/unisa/pharmaweb/control/filter/GlobalDataFilter.java
@@ -0,0 +1,25 @@
+package it.unisa.pharmaweb.control.filter;
+
+import java.io.IOException;
+
+import it.unisa.pharmaweb.model.dao.RecensioneDAO;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+
+@WebFilter("/*")
+public class GlobalDataFilter implements Filter {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        // Carichiamo le recensioni solo per le richieste GET (pagine)
+        if (req.getMethod().equalsIgnoreCase("GET")) {
+            RecensioneDAO dao = new RecensioneDAO();
+            request.setAttribute("latestReviews", dao.getLatestReviews(3));
+        }
+        chain.doFilter(request, response);
+    }
+}
